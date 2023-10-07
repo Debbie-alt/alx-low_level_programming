0b188353181308967cea6c9b586a7b9c6d39ddc5
Fayemi Funmilayo <busila976@gmail.com>
2023-10-06 18:49:45 -0700

    A printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A functions that prints without using puts
+ * Return: 0(correct)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+};
